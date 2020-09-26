2626fff8ba84068e62d63f52fa2d5ac77a819f68
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-26 09:35:39 -0400

    basic functionality completed

--- script.js
+++ script.js
@@ -12,33 +12,42 @@ function imageMode(color) {
   image3.src = `img/undraw_nature_${color}.svg`;
 }
 
-// dark mode styles
-function darkMode() {
-  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageMode('dark');
-}
-
-// light mode styles
-function lightMode() {
-  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageMode('light');
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = isDark
+    ? 'rgb(0 0 0 / 50%)'
+    : 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+  isDark
+    ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+    : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  isDark ? imageMode('dark') : imageMode('light');
 }
 
 // switch theme
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
-    darkMode();
+    localStorage.setItem('theme', 'dark');
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
-    lightMode();
+    localStorage.setItem('theme', 'light');
+    toggleDarkLightMode(false);
   }
 }
 // event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+//check local storage for tehem
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme == 'dark') {
+    toggleSwitch.checked = true;
+    toggleDarkLightMode(true);
+  }
+}
